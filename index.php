6265cb32d729bd1419d8b08933bd2ee54a979f12
metjeanjunior <metjeanjunior@gmail.com>
2016-04-13 11:06:43 -0400

    added temp homepage

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Book Marketplace</title>
+	<style type="text/css">
+		body { background-color: turquoise; }
+	</style>
+</head>
+<body>
+	<h1>Thanks for visiting so soon!<br>
+	But we are under construction<br>
+	Here is something to keep you happy until launch :)<h1><br>
+	<img src="cat.jpeg">
+	<img src="dog.jpeg">
+</body>
+</html>
