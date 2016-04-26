4ce3ab3790efd50773d384cc3b628ae556dcaede
metjeanjunior <metjeanjunior@gmail.com>
2016-04-25 20:46:20 -0400

    added loginCheck and bootstrap error message to login page

--- login.php
+++ login.php
@@ -3,6 +3,7 @@
 	<head>
 		<title>Book Marketplace</title>
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.2/jquery.min.js"></script>
+		<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
 		<script src="js/index.js"></script>
 		<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css" />
 		<link rel="stylesheet" type="text/css" href="css/body.css">
@@ -13,15 +14,27 @@
 		But we are under construction
 		Here is something to keep you happy until launch :)</marquee><br>
 
-		<!-- <div id="login-box"> -->
 		<div class="container">
 
-			<form class="form-signin">
+			<form class="form-signin" method="post" action="php/loginCheck.php">
 		        <h2 class="form-signin-heading">Please sign in</h2>
 		        <label for="inputEmail" class="sr-only">Email address</label>
 		        <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
 		        <label for="inputPassword" class="sr-only">Password</label>
 		        <input type="password" id="inputPassword" class="form-control" placeholder="Password" required>
+
+				<?php
+					if (isset($_GET['badPass']) and $_GET['badPass'] = true) 
+					{
+						?>
+						<div class="alert alert-danger fade in">
+						    <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
+						    <strong>Danger!</strong> This alert box could indicate a dangerous or potentially negative action.
+						</div>
+						<?php
+					}
+				?>
+
 		        <div class="checkbox">
 		          <label>
 		            <input type="checkbox" value="remember-me"> Remember me
