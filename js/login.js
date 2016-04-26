65b6ea1dc47e98fa2af5b79c1c29f15e4076c47d
metjeanjunior <metjeanjunior@gmail.com>
2016-04-26 00:29:44 -0400

    minor restructure + init viewItem page

---
+++ js/login.js
@@ -0,0 +1,33 @@
+// inspired by: http://bootsnipp.com/snippets/featured/modal-login-with-jquery-effects
+
+$(function() {
+	var $formLogin = $("#login-form");
+	var $formRegister = $("#register-form");
+	var $formForgot = $("#forgot-form");
+
+	("#login-forgot").click(function(){
+		$("#div-forms").toggle();
+		$formLogin.toggle();
+		$formForgot.toggle();
+	});
+	("#login-register").click(function(){
+		$formLogin.toggle();
+		$formRegister.toggle();
+	});
+	("#forgot-login").click(function(){
+		$formForgot.toggle();
+		$formLogin.toggle();
+	});
+	("#forgot-register").click(function(){
+		$formForgot.toggle();
+		$formRegister.toggle();
+	});
+	("#register-login").click(function(){
+		$formRegister.toggle();
+		$formLogin.toggle();
+	});
+	("#register-forgot").click(function(){
+		$formRegister.toggle();
+		$formForgot.toggle();
+	});
+});
