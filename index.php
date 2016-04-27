cc598965b6c8a03011c2c78b6e1fd5c34b49ab26
metjeanjunior <metjeanjunior@gmail.com>
2016-04-27 11:23:23 -0400

    added redirecting to searchResutl from home page

--- index.php
+++ index.php
@@ -24,18 +24,20 @@
 
 	 	<div class="container">
 	 		<div class="row">
-	 	        <div class="col-sm-6 col-sm-offset-3">
-	 	            <div id="imaginary_container"> 
-	 	                <div class="input-group stylish-input-group">
-	 	                    <input type="text" class="form-control"  placeholder="Search" >
-	 	                    <span class="input-group-addon">
-	 	                        <button type="submit">
-	 	                            <span class="glyphicon glyphicon-search"></span>
-	 	                        </button>  
-	 	                    </span>
-	 	                </div>
-	 	            </div>
-	 	        </div>
+	 			<form method="get" action="php/searchResults.php">
+		 	        <div class="col-sm-6 col-sm-offset-3">
+		 	            <div id="imaginary_container"> 
+		 	                <div class="input-group stylish-input-group">
+		 	                    <input type="text" class="form-control"  placeholder="Search" >
+		 	                    <span class="input-group-addon">
+		 	                        <button type="submit" onclick="">
+		 	                            <span class="glyphicon glyphicon-search" aria-hidden="true"></span>
+		 	                        </button>  
+		 	                    </span>
+		 	                </div>
+		 	            </div>
+		 	        </div>
+	 			</form>
 	 		</div>
 	 	</div>
 	 	
@@ -46,3 +48,4 @@
  	    </footer>
 	</body>
 </html>
+
