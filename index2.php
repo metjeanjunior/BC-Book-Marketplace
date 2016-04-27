214ce72a867e5992861309546bc535b9fe1021a1
metjeanjunior <metjeanjunior@gmail.com>
2016-04-27 11:23:33 -0400

    added redirecting to searchResutl from home page

---
+++ index2.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Book Marketplace</title>
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.2/jquery.min.js"></script>
+		<script src="js/marquee.js"></script>
+		<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css" />
+		<link rel="stylesheet" type="text/css" href="css/index.css">
+		<link rel="stylesheet" type="text/css" href="css/global.css">
+	</head>
+
+	<body>
+		<marquee>Thanks for visiting so soon!
+		But we are under construction
+		Here is something to keep you happy until launch :)</marquee><br>
+
+		<div class="header">
+			<a  id="login-ling" href="login.php">Login</a>
+			<a id="add-link" href="php/addBook.php">Lend/Sell</a>
+	 	</div>
+
+
+	 	<div id="logo">BC's MarketPlace</div>
+
+	 	<div class="container">
+	 		<div class="row">
+	 			<form method="get" action="php/searchResults.php">
+		 	        <div class="col-sm-6 col-sm-offset-3">
+		 	            <div id="imaginary_container"> 
+		 	                <div class="input-group stylish-input-group">
+		 	                    <input type="text" class="form-control"  placeholder="Search" >
+		 	                    <span class="input-group-addon">
+		 	                        <button type="submit" onclick="">
+		 	                            <span class="glyphicon glyphicon-search" aria-hidden="true"></span>
+		 	                        </button>  
+		 	                    </span>
+		 	                </div>
+		 	            </div>
+		 	        </div>
+	 			</form>
+	 		</div>
+	 	</div>
+	 	
+	 	<footer class="panel-footer footer">
+	 	    <div class="container">
+		 	    <p class="text-muted">Designed by the people of BC for the People of BC</p>
+	 	    </div>
+ 	    </footer>
+	</body>
+</html>
+
---
+++ php/searchResults.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Search Results</title>
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.2/jquery.min.js"></script>
+		<script src="../js/marquee.js"></script>
+		<link rel="stylesheet" type="text/css" href="../css/bootstrap.min.css" />
+		<link rel="stylesheet" type="text/css" href="../css/global.css">
+	</head>
+	<body>
+		<marquee>Thanks for visiting so soon!
+		But we are under construction
+		Here is something to keep you happy until launch :)</marquee><br>
+	</body>
+</html>
