64c96fded3f1b5bfdadcbfc210bff70e84a7baa5
metjeanjunior <metjeanjunior@gmail.com>
2016-04-28 00:49:01 -0400

    more crawling update + transaction page init

--- php/isbnLooker.php
+++ php/isbnLooker.php
@@ -7,11 +7,10 @@
 	$content = file_get_contents($page);
 	$pattern = '$http:\/\/www.amazon.com\/([A-Za-z]*-*)*\/dp\/1338097679\/ref=.*"><img$';
 	preg_match_all ($pattern, $content, $res);
-	// echo "The pattern is " . htmlentities($pattern) . " <br />\n";
 	if (is_null($res[0][0]))
 		echo "empty(var)";
-	// echo $res[0][0];
 	$page = $res[0][0];
+	// echo $page;
 
 	// $titlePattern = '$<span id="productTitle" class="a-size-large">[\S- ]*<\/span>$';
 	// $page = $res[0][0];
@@ -23,12 +22,16 @@
 	// $bookTitle = $res[0][0];
 	// echo "the book title is $bookTitle";	
 
-	$titlePattern = '<title>[\S ]*<\/title>';
-	$page = $res[0][0];
-	// echo ;
+	$titlePattern = '<title>[\S ]*';
+	// echo $titlePattern;
+	// $page = $res[0][0];
 	$content = file_get_contents($page);
+	// echo $content;
 	preg_match_all ($titlePattern, $content, $res);
+	echo $res[0];
 	if (is_null($res[0][0]))
 		echo "empty(var)";
 	else
-		echo $res[0][0].'test';
+		$title = $res[0][0].'test';
+	// echo sizeof($tittle);
+	// echo $tittle[2];
