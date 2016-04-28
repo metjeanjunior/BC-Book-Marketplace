d19a33d678ac79e7085c29432230b3118cf08834
chiangcb <chiangcb@bc.edu>
2016-04-27 22:40:16 -0400

    Main page/Searchbar for Instant Search updated

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
 		<title>Book Marketplace</title>
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.2/jquery.min.js"></script>
 		<script src="js/marquee.js"></script>
+		<script src="js/searchbar.js"></script>
 		<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css" />
 		<link rel="stylesheet" type="text/css" href="css/index.css">
 		<link rel="stylesheet" type="text/css" href="css/global.css">
@@ -27,7 +28,8 @@
 	 	        <div class="col-sm-6 col-sm-offset-3">
 	 	            <div id="imaginary_container"> 
 	 	                <div class="input-group stylish-input-group">
-	 	                    <input type="text" class="form-control"  placeholder="Search" >
+	 	                    <input type="text" class="form-control"  placeholder="Search" 
+	 	                    name="search-bar" id="search-bar-1" onkeyup="search(this.value)">
 	 	                    <span class="input-group-addon">
 	 	                        <button type="submit">
 	 	                            <span class="glyphicon glyphicon-search"></span>
@@ -36,6 +38,10 @@
 	 	                </div>
 	 	            </div>
 	 	        </div>
+	 		</div>
+	 	</div>
+	 	<div class="container">
+	 		<div id="result">
 	 		</div>
 	 	</div>
 	 	
