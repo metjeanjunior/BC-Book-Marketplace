c09db161f27f329f3f0fe5fc5a9b1890c1786de4
chiangcb <cchiang@Chriss-MacBook-Pro.local>
2016-04-30 15:11:10 -0400

    Not fixed entirely

--- php/searchResults.php
+++ php/searchResults.php
@@ -7,50 +7,11 @@
         <script src="../js/marquee.js"></script>
         <script src="../js/viewRes.js"></script>
         <script src="../js/searchbar.js"></script>
-        <link rel="stylesheet" type="text/css" href="../css/bootstrap.min.css" />
-        <link rel="stylesheet" type="text/css" href="../css/global.css">
-        <link rel="stylesheet" type="text/css" href="../css/index.css">
-        <link rel="stylesheet" type="text/css" href="../css/viewRes.css">
+        <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css" />
+        <link rel="stylesheet" type="text/css" href="css/global.css">
+        <link rel="stylesheet" type="text/css" href="css/index.css">
+        <link rel="stylesheet" type="text/css" href="css/viewRes.css">
     </head>
-    <body>
-        <marquee>Thanks for visiting so soon!
-        But we are under construction
-        Here is something to keep you happy until launch :)</marquee><br>
-
-        <div class="container">
-            <div class="row">
-                <form method="get" action="searchResults.php">
-                    <div class="col-sm-6 col-sm-offset-3">
-                        <div id="imaginary_container"> 
-                            <div class="input-group stylish-input-group">
-                                <input type="text" class="form-control"  placeholder="Search" 
-                                    name="search-bar" id="search-bar-1" onkeyup="search(this.value)">
-                                <span class="input-group-addon">
-                                    <button type="submit" onclick="">
-                                        <span class="glyphicon glyphicon-search" aria-hidden="true"></span>
-                                    </button>  
-                                </span>
-                            </div>
-                        </div>
-                    </div>
-                </form>
-            </div>
-        </div>
-
-        <div class="container">
-            <div class="well well-sm res-header">
-                <strong>Category Title</strong>
-                <div class="btn-group">
-                    <a href="#" id="list" class="btn btn-default btn-sm"><span class="glyphicon glyphicon-th-list">
-                    </span>List</a> <a href="#" id="grid" class="btn btn-default btn-sm"><span
-                        class="glyphicon glyphicon-th"></span>Grid</a>
-                </div>
-            </div>
-            <div id="products" class="row list-group">
-                
-            </div>
-        </div>
-    </body>
 </html>
 <?php
     $tofind = $_GET['search-bar'];
