29439ba73802bbf2dc4f048c4f0555e6947d3071
Colin Gallaway <gallawac@bc.edu>
2016-04-30 17:46:43 -0400

    Implemented viewBook.php
    edited viewBook.php
    added viewBook.css

--- php/viewBook.php
+++ php/viewBook.php
@@ -1,3 +1,6 @@
+<?php
+include("../include/dbconn.php");
+?>
 <!DOCTYPE html>
 <html>
 	<head>
@@ -6,8 +9,9 @@
 		<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
 		<script src="../js/marquee.js"></script>
 		<script src="../js/searchbar.js"></script>
-		<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css" />
+		<link rel="stylesheet" type="text/css" href="../css/bootstrap.min.css" />
 		<link rel="stylesheet" type="text/css" href="../css/global.css">
+		<link rel="stylesheet" type="text/css" href="../css/viewBook.css">
 	</head>
 	<body>
 		<marquee>Thanks for visiting so soon!
@@ -19,27 +23,60 @@
 	// if(!isset($_GET['itemId']))
 	// 	header("Location: ../index.php");
 
-	
-	// $ibsn = $_GET['ibsn'];
-	$ibsn = '2147483647';
+
+	$bookID = $_POST['bookID'];
+	//$ibsn = '2147483647';
 
 	$dbc = connectToDB();
 	if ($dbc == 'bad')
 		header("Location: viewBook.php?error=true");;
 
-	$query = "select * from book where book_ibsn = $ibsn";
+	$query = "select * from book where book_id = '$bookID'";
 	$result = performQuery($dbc, $query);
 	$row = mysqli_fetch_array($result,MYSQLI_NUM);
-	echo $row[1];
+
+	$bookSellerID = $row[5];
+
+	$bookName = $row[2];
+	$bookISBN = $row[1];
+	$bookDescription = $row[3];
+	$bookPrice = $row[4];
+	$bookCondition = $row[6];
+	$bookDate = $row[7];
+
+	$query = "select * from customer where customer_id = '$bookSellerID'";
+	$result = performQuery($dbc, $query);
+	$row = mysqli_fetch_array($result,MYSQLI_NUM);
+	$SellerEmail = $row[3];
+
+
+
+	//echo $bookName;
+	//echo $SellerEmail;
+	//echo $bookISBN;
+	//echo $bookDescription;
+	//echo $bookPrice;
+	//echo $bookCondition;
+	//echo $bookDate;
 
 	?>
-		<div class="container">
-			<div class="row">
-				
-			</div>
-		</div>
+	<div class="jumbotron">
+	  <h1><?php echo $bookName; ?></h1>
+	  <p>
+	  Seller: <?php echo $SellerEmail; ?> <br>
+	  ISBN: <?php echo $bookISBN; ?> <br>
+	  Price: <?php echo $bookPrice; ?> <br>
+	  Condition: <?php echo $bookCondition; ?> <br>
+	  Added on: <?php echo $bookDate; ?> <br><br>
+	  <?php echo $bookDescription; ?>
+	  </p>
+	  <p>
+	  <form method="post" action="transaction.php">
+	  	<input type="text" name="bookID" value="<?php echo $bookID; ?>" hidden="hidden">
+	   	<input type="submit" class="btn btn-primary btn-lg" value="Perform Transaction">
+      </form>
+	</div>
 	<?php
-
 	function connectToDB()
 	{
 		$dbc= @mysqli_connect("localhost", "metelusj", "23JD5h5z", "metelusj") or
@@ -55,7 +92,8 @@
 
 	function performQuery($dbc, $query)
 	{
-		//echo "My query is >$query< <br />";	
+		//echo "My query is >$query< <br />";
 		$result = mysqli_query($dbc, $query) or die("bad query: ".mysqli_error($dbc));
 		return ($result);
 	}
+	?>
