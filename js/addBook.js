b5dc5d91eb2a1b85bc92270c6f3b0c840822b8d5
chiangcb <cchiang@Chriss-MacBook-Pro.local>
2016-04-30 22:32:18 -0400

    javascript code ready to implement
    even a small amount of code can take a lot of experimentation...

--- js/addBook.js
+++ js/addBook.js
@@ -3,12 +3,22 @@ function fillForm()
 	$.getJSON("../php/newIbsnCrawler.php?ibsn="+$("#book-ibsn").val(), 
 	function(data)
 	{
-		$("body").append(data.items[0].volumeInfo.title + "<br>");		
-		$("body").append(data.totalItems);
-		// $.each(data, function(i,info)
-		// {
-		// 	$("body").append(data.kind);
-		// 	$("body").append("this is a test");
-		// });
+		$("#standby").html("Looking up ISBN...Please wait...")
+		if (data.totalItems >= 1) {
+			setTimeout(function() {
+				$("#standby").html(data.items[0].volumeInfo.title + "<br>");		
+				$("#standby").append(data.items[0].volumeInfo.description + "<br>");
+				$("input[name=book-name]").val(data.items[0].volumeInfo.title);			
+			}, 0);
+
+			// $.each(data, function(i,info)
+			// {
+			// 	$("body").append(data.kind);
+			// 	$("body").append("this is a test");
+			// });
+		}
+		else {
+			$("#standby").append("The ISBN number you typed is either invalid or has not been assigned yet. Please try again. <br>")
+		}
 	});
 }
